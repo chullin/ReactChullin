56465d1d2a30c6e0cac5593a2b27ab3b23c89519
chullin <fg6ts15@gmail.com>
2026-07-19 01:08:33 +0800

    feat(nav): link market watch page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -24,6 +24,7 @@ export default function Navbar() {
 
   const playgroundLinks = [
     { name: t('nav.imageCompressor'), href: '/image-compressor' },
+    { name: 'Market Watch', href: '/market-watch' },
     { name: t('nav.stockWatchlist'), href: '/tool/stock/stock-watchlist' },
     { name: t('nav.snake'), href: '/snake' },
     { name: t('nav.vocabQuiz'), href: '/vocab-quiz' },
